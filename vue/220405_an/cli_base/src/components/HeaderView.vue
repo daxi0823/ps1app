8f17e3eb0a691b56aae071dd9d08482ee9418d2b
ryohei <daxi0823@gmail.com>
2022-04-05 17:01:42 +0900

    vue import html/css

---
+++ vue/220405_an/cli_base/src/components/HeaderView.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+      <div class="container">
+        <h2 class="logo"><a href="./index.html"><img src="/images/logo.png" alt="Cafe Du Ando" width="800" height="205"></a></h2>
+        <div class="gnav_wrapper">
+          <input type="checkbox" id="nav_input" class="nav_hidden">
+          <label for="nav_input" id="menu_btn" class="hamburger_menu active">
+            <span></span>
+            <span></span>
+            <span></span>
+          </label>
+          <nav id="gnav">
+            <ul>
+              <li><a href="#posi">Positon</a></li>
+              <li><a href="#menu">Menu</a></li>
+              <li><a href="#desc">About</a></li>
+              <li><a href="#accordion">Accordion</a></li>
+              <li><a href="#table">Table</a></li>
+              <li><a href="#footer__top">Contact</a></li>
+            </ul>
+          </nav>
+        </div><!--gnav_wrapper-->
+      </div><!--gnav_wrapper-->
+    </header>
+  </template>
