ffba994f23e871ec955a5ace3f4e0f7674ea6fa1
daxi0823 <daxi0823@gmail.com>
2022-04-20 12:34:44 +0900

    first commit

---
+++ portfolio/datingtest02/src/views/AboutView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="result">
+    <div class="searchWrapper">
+      <h1>結果</h1>
+      <p>希望条件</p>
+      <ul>
+        <li>日付：
+          <select name="" id="">
+            <option value="">4/15</option>
+          </select>
+        </li>
+        <li>出発時間：
+          <select name="" id="">
+            <option value="10:00">10:00</option>
+          </select>
+        </li>
+        <li>出発駅：<select name="" id=""></select></li>
+        <li>所要時間：<select name="" id=""></select></li>
+        <li>移動時間（片道）<select name="" id=""></select></li>
+      </ul>
+    </div>
+    <!-- /searchWrapper -->
+    <div class="resultWrapper">
+      
+    </div>
+    <!-- /.resultWrapper -->
+  </div>
+</template>
+
+
+<style scoped>
+  ul{
+    list-style-type: none;
+    display: flex;
+  }
+</style>
